f01177a1c2e663d8bd337ccc0f6f46e9c8b7a509
ajays7488 <42312922+ajays7488@users.noreply.github.com>
2019-02-21 16:01:43 +0530

    Update main.js
    updated js

--- src/main.js
+++ src/main.js
@@ -1,54 +1,27 @@
-require('../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css');
-
-var SDK = require('blocksdk');
-var sdk = new SDK();
-
-fetch('/appID').then(function (res) {
-	return res.text();
-}).then(function (appID) {
-	sdk.triggerAuth(appID);
-});
 
 
-var post = function (url, data, cb) {
-	fetch('/proxy/' + url, {
-		method: 'POST',
-		body: JSON.stringify(data),
-		headers: self.headers,
-		credentials: 'include',
-		mode: 'no-cors'
-	}).then(function (res) {
-		if (res.status === 401) {
-			setTimeout(function () {
-				post(url, data, cb);
-			}, 100);
-			return Promise.reject('401');
+var BlockSDK = require('blocksdk');
+if (window.self === window.top) {
+	document.body.innerText = 'This application is for use in the Salesforce Marketing Cloud Content Builder Editor only.';
+} else {
+	var sdk = new BlockSDK();
+	sdk.getContent(function (content) {
+		var quill = new Quill('#editor-container', {
+			theme: 'snow'
+		});
+		quill.root.innerHTML = content;
+		function saveText() {
+			var html = quill.root.innerHTML;
+			sdk.setContent(html);
+			sdk.setSuperContent('This is awesome content: ' + html);
+
+			sdk.getData(function (data) {
+				var numberOfEdits = data.numberOfEdits || 0;
+				sdk.setData({
+					numberOfEdits: numberOfEdits + 1
+				});
+			});
 		}
-		return res.json();
-	}).then(function (data) {
-		cb(data);
+		quill.on('text-change', saveText);
 	});
-};
-
-window.setContent = function (url) {
-	sdk.setContent('<img src="' + url + '"></img>');
-};
-
-var getImages = function () {
-	post('asset/v1/content/assets/query', {
-		query: {
-			property: "assetType.id",
-			simpleOperator: "in",
-			value: [20, 22, 23, 28]
-		}
-	}, function (data) {
-		var dom = '';
-		for (var key in data.items) {
-			var src = data.items[key].fileProperties.publishedURL;
-			dom += '<div style="display:inline-block;width:50px;border:1px solid white;height:50px"><img style="width:50px;max-height:50px;background-color:lightgray;" onclick="setContent(\'' + src + '\')" src="' + src + '"></img></div>';
-		}
-		document.getElementById('workspace').innerHTML = dom;
-	});
-};
-
-getImages();
+}
